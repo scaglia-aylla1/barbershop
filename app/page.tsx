e7a28300b55b3a0c86cecd3efd6e378b3ef42f6f
scaglia-aylla1 <aylla@scaglia.com.br>
2025-11-12 14:40:31 -0300

    feat: add barbershop item component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,24 @@ import Header from "./components/ui/header";
 import Image from "next/image";
 import banner from "../public/banner.png";
 import BookingItem from "./components/ui/booking-item";
+import { prisma } from "@/lib/prisma";
+import BarbershopItem from "./components/barbershop-item";
 
-const Home = () => {
+const Home = async () => {
+  const recommendedBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "asc",
+    }
+  });
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    }
+  });
   return (
     <main>
       <Header />
-      <div className="space-y-4 px-5">
+      <div className="space-y-4 p-5">
         <SearchInput />
         <Image
           src={banner}
@@ -25,6 +37,22 @@ const Home = () => {
           barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
           date={new Date()}
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {recommendedBarbershops.map((barbershop) => (
+               <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+        </div>
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {popularBarbershops.map((barbershop) => (
+               <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+        </div>
       </div>
     </main>
   );
